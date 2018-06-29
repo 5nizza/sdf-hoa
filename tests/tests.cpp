baf4c63478a78d9584fe843e2d888559bf1aefee
ayrat <ayrat.khalimov@gmail.com>
2018-06-29 15:36:00 +0300

    tests: checking in both phases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -13,71 +13,86 @@
 using namespace std;
 
 
-const vector<string> unreal_specs =
+struct SpecParam
 {
-    "round_robin_arbiter_unreal1.tlsf",
-    "round_robin_arbiter_unreal2.tlsf",
-    "prioritized_arbiter_unreal1.tlsf",
-    "prioritized_arbiter_unreal2.tlsf",
-    "prioritized_arbiter_unreal3.tlsf",
-    "parameterized/load_balancer_unreal1.tlsf",
-    "mealy_moore_unreal.tlsf",
-    "simple_arbiter_unreal1.tlsf",
-    "simple_arbiter_unreal2.tlsf",
-    "simple_arbiter_unreal3.tlsf",
-    "detector_unreal.tlsf",
-    "full_arbiter_unreal1.tlsf",
-    "full_arbiter_unreal2.tlsf"
+    const string name;
+    const bool is_real;
+
+    SpecParam(const string& name_, bool is_real_) : name(name_), is_real(is_real_) { }
 };
 
 
-const vector<string> real_specs =
+const vector<SpecParam> specs =
 {
-    "load_balancer_real2.tlsf",
-    "simple_arbiter.tlsf",
-    "detector.tlsf",
-    "full_arbiter.tlsf",
-    "load_balancer.tlsf",
-    "round_robin_arbiter.tlsf",
-    "round_robin_arbiter2.tlsf",
-    "prioritized_arbiter.tlsf",
-    "mealy_moore_real.tlsf"
+    SpecParam("round_robin_arbiter_unreal1.tlsf", false),
+    SpecParam("round_robin_arbiter_unreal2.tlsf", false),
+    SpecParam("prioritized_arbiter_unreal1.tlsf", false),
+    SpecParam("prioritized_arbiter_unreal2.tlsf", false),
+    SpecParam("prioritized_arbiter_unreal3.tlsf", false),
+    SpecParam("load_balancer_unreal1.tlsf", false),
+    SpecParam("mealy_moore_unreal.tlsf", false),
+    SpecParam("simple_arbiter_unreal1.tlsf", false),
+    SpecParam("simple_arbiter_unreal2.tlsf", false),
+    SpecParam("simple_arbiter_unreal3.tlsf", false),
+    SpecParam("detector_unreal.tlsf", false),
+    SpecParam("full_arbiter_unreal1.tlsf", false),
+    SpecParam("full_arbiter_unreal2.tlsf", false),
+
+    SpecParam("load_balancer_real2.tlsf", true),
+    SpecParam("simple_arbiter.tlsf", true),
+    SpecParam("detector.tlsf", true),
+    SpecParam("full_arbiter.tlsf", true),
+    SpecParam("load_balancer.tlsf", true),
+    SpecParam("round_robin_arbiter.tlsf", true),
+    SpecParam("round_robin_arbiter2.tlsf", true),
+    SpecParam("prioritized_arbiter.tlsf", true),
+    SpecParam("mealy_moore_real.tlsf", true)
 };
 
 
 /// @returns: return_code of the tool execution
-bool execute_tool(const string& name)
+int execute_tool(const string& name, bool do_unreal_check, uint k)
 {
-    auto rc = sdf::run("./specs/" + name, true, {1, 2, 3}, "");
-    cout << name << ", rc: " << rc << endl;
-    return true;
+    return sdf::run("./specs/" + name, do_unreal_check, {k}, "");
 }
 
 
-class TestUnreal : public ::testing::TestWithParam<string> { };
-class TestReal : public ::testing::TestWithParam<string> { };
+class TestSpec : public ::testing::TestWithParam<SpecParam> { };
 
 
-TEST_P(TestUnreal, check_unreal)
-{   /// ensure it returns realizable when checking unrealizability of unrealizable specs
-    EXPECT_TRUE(execute_tool(GetParam()));
+TEST_P(TestSpec, check_unreal)
+{
+    auto spec = GetParam();
+
+    auto status = execute_tool(spec.name, true, 4);
+
+    if (spec.is_real)
+        EXPECT_EQ(SYNTCOMP_RC_UNKNOWN, status);
+
+    if (!spec.is_real)
+        EXPECT_EQ(SYNTCOMP_RC_UNREAL, status);
 }
 
 
-TEST_P(TestUnreal, check_real)
-{   /// ensure it returns unknown when checking realizability of unrealizable specs
-    EXPECT_TRUE(execute_tool(GetParam()));
+TEST_P(TestSpec, check_real)
+{
+    auto spec = GetParam();
+
+    auto status = execute_tool(spec.name, false, 4);
+
+    if (spec.is_real)
+        EXPECT_EQ(SYNTCOMP_RC_REAL, status);
+
+    if (!spec.is_real)
+        EXPECT_EQ(SYNTCOMP_RC_UNKNOWN, status);
 }
 
 
-INSTANTIATE_TEST_CASE_P(Unreal,
-                        TestUnreal,
-                        ::testing::ValuesIn(unreal_specs));
+INSTANTIATE_TEST_CASE_P(RealAndUnreal,
+                        TestSpec,
+                        ::testing::ValuesIn(specs));
 
 
-INSTANTIATE_TEST_CASE_P(Real,
-                        TestReal,
-                        ::testing::ValuesIn(real_specs));
 // PAST:
 // - just added the tools, using parameterized tests, see:
 //   https://stackoverflow.com/questions/19160244/create-tests-at-run-time-google-test
